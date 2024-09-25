8b0fb16bae29eff21fb56b6daaa02e09a6dc95fc
rarlala <wtw9611@gmail.com>
2024-09-26 00:50:24 +0900

    bugfix: jsx-a11y/click-events-have-key-events
    https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/click-events-have-key-events.md

--- src/components/header/myMenuItem.tsx
+++ src/components/header/myMenuItem.tsx
@@ -8,14 +8,22 @@ type Props = {
 
 const MyMenuItem = memo(function MyMenuItem({ menu }: Props) {
   return (
-    <li
-      className={`hidden md:block ${menu?.style}`}
-      onClick={() => menu.callback && menu.callback()}
-    >
-      <Link href={menu.link} className="flex gap-1 items-center">
-        {menu.icon}
-        <span className="hidden xl:inline-block">{menu.title}</span>
-      </Link>
+    <li className={`hidden md:block ${menu?.style}`}>
+      {menu.callback ? (
+        <button
+          type="button"
+          onClick={menu.callback}
+          className="flex gap-1 items-center"
+        >
+          {menu.icon}
+          <span className="hidden xl:inline-block">{menu.title}</span>
+        </button>
+      ) : (
+        <Link href={menu.link} className="flex gap-1 items-center">
+          {menu.icon}
+          <span className="hidden xl:inline-block">{menu.title}</span>
+        </Link>
+      )}
     </li>
   );
 });
