48f65d3af19d826f0743395861bbd98e870252b3
rarlala <wtw9611@gmail.com>
2024-10-15 00:14:26 +0900

    feat: add product mock data

---
+++ src/types/product.ts
@@ -0,0 +1,26 @@
+export type ProductDataType = {
+  brandInfo: {
+    name: string;
+    subCopy: string;
+    brandId: number;
+  };
+  productInfo: {
+    itemId: number;
+    itemName: string;
+    isLiked: boolean;
+    avgReviewPoint: number;
+    reviewCount: number;
+    saleInfo: {
+      consumerPrice: number;
+      totalSalePercent: number;
+      totalSalePrice: number;
+    };
+    options: {
+      [key: string]: string[];
+    };
+  };
+  deliveryInfo: {
+    estimatedDeliveryTime: string;
+    deliveryFee: 'free' | number;
+  };
+};
