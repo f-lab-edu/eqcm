80b182daff0a77ce03510a5cf600f2c54f54a857
rarlala <wtw9611@gmail.com>
2024-10-21 19:45:42 +0900

    refactor: modified to receive in children format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,15 @@ import BoxSection, { BoxSectionSkeleton } from '@/components/main/boxSection';
 import ViewMoreButton from '@/components/main/viewMoreButton';
 import { fetchMainData } from '@/fetch/main';
 import { Suspense } from 'react';
+import Banner from '@/components/main/banner';
+import BoxBanner from '@/components/main/boxBanner';
+import BoxSlider from '@/components/main/boxSlider';
+import {
+  BannerType,
+  BoxBannerType,
+  BoxSliderType,
+  MainComponentsType,
+} from '@/types/main';
 
 export default function Home() {
   return (
@@ -39,7 +48,26 @@ function MainContent() {
     <div className="flex-1 font-pretendard">
       <div className="flex flex-col md:flex-row">
         <BannerSection data={data.banners} />
-        <BoxSection data={data.boxes} />
+        <BoxSection>
+          {data.boxes?.map((box, idx) => {
+            if (box.type === MainComponentsType.BANNER) {
+              return (
+                <Banner
+                  key={idx}
+                  data={box as BannerType}
+                  style="desktop-only"
+                />
+              );
+            }
+            if (box.type === MainComponentsType.BOX_BANNER) {
+              return <BoxBanner key={idx} data={box as BoxBannerType} />;
+            }
+            if (box.type === MainComponentsType.BOX_SLIDER) {
+              return <BoxSlider key={idx} data={box as BoxSliderType} />;
+            }
+            return null;
+          })}
+        </BoxSection>
       </div>
       <ViewMoreButton />
     </div>
--- src/components/main/boxSection.tsx
+++ src/components/main/boxSection.tsx
@@ -1,48 +1,18 @@
 'use client';
 
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
-import BoxRenderer from './boxRenderer';
-import Banner, { BannerSkeleton } from './banner';
-import BoxBanner, { BoxBannerSkeleton } from './boxBanner';
-import BoxSlider from './boxSlider';
-import {
-  BannerType,
-  BoxBannerType,
-  BoxSliderType,
-  MainComponentsType,
-} from '@/types/main';
+import { BannerSkeleton } from './banner';
+import { BoxBannerSkeleton } from './boxBanner';
 
 type Props = {
-  data: (BannerType | BoxBannerType | BoxSliderType)[] | undefined;
+  children: React.ReactNode;
 };
 
-const BoxSection = ({ data }: Props) => {
-  if (data === undefined) {
-    return null;
-  }
-
+const BoxSection = ({ children }: Props) => {
   return (
     <section className="relative w-full md:w-[56%]">
       <ResponsiveMasonry columnsCountBreakPoints={{ 300: 1, 719: 2 }}>
-        <Masonry>
-          {data.map((box, idx) => (
-            <BoxRenderer key={idx}>
-              {box.type === MainComponentsType.BANNER && (
-                <Banner
-                  key={idx}
-                  data={box as BannerType}
-                  style="desktop-only"
-                />
-              )}
-              {box.type === MainComponentsType.BOX_BANNER && (
-                <BoxBanner key={idx} data={box as BoxBannerType} />
-              )}
-              {box.type === MainComponentsType.BOX_SLIDER && (
-                <BoxSlider key={idx} data={box as BoxSliderType} />
-              )}
-            </BoxRenderer>
-          ))}
-        </Masonry>
+        <Masonry>{children}</Masonry>
       </ResponsiveMasonry>
       <div className="absolute w-[1px] h-full bg-[#ccc] top-0 right-[50%] hidden lg:block" />
     </section>
